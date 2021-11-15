028f936276aaa1f4a40cfe230b059664acf0d2f1
wespe <m.dobrynin@raccoonsoft.ru>
2021-11-15 20:52:19 +0100

    beans/naming/lesson-2: Java-configuration - simplify view

--- src/main/java/com/jetbulb/spring/overview/beans/naming/SpringApplication.java
+++ src/main/java/com/jetbulb/spring/overview/beans/naming/SpringApplication.java
@@ -1,12 +1,15 @@
 package com.jetbulb.spring.overview.beans.naming;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Arrays;
 
 public class SpringApplication {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("com.jetbulb.spring.overview.beans.naming");
+        ApplicationContext context = new AnnotationConfigApplicationContext(
+                "com.jetbulb.spring.overview.beans.naming"
+        );
 
         Arrays.stream(context.getBeanDefinitionNames())
                 .forEach(name -> System.out.printf("Id: %s%n%n", name));
