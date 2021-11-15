f6a3967a73b76c206f02f4e6f22979435387fb95
wespe <m.dobrynin@raccoonsoft.ru>
2021-11-15 21:15:01 +0100

    beans/naming/lesson-4: troubleshooting - simplify view

--- src/main/java/com/jetbulb/spring/overview/beans/naming/SpringApplication.java
+++ src/main/java/com/jetbulb/spring/overview/beans/naming/SpringApplication.java
@@ -1,12 +1,16 @@
 package com.jetbulb.spring.overview.beans.naming;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SpringApplication {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(JavaConfiguration.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(JavaConfiguration.class);
 
-        context.getBeansOfType(BBeanA.class).forEach((k, v) -> System.out.printf("Id: %s%nBean: %s%n%n", k, v));
-        context.getBeansOfType(BBBeanA.class).forEach((k, v) -> System.out.printf("Id: %s%nBean: %s%n%n", k, v));
+        context.getBeansOfType(BBeanA.class)
+                .forEach((k, v) -> System.out.printf("Id: %s%nBean: %s%n%n", k, v));
+
+        context.getBeansOfType(BBBeanA.class)
+                .forEach((k, v) -> System.out.printf("Id: %s%nBean: %s%n%n", k, v));
     }
 }
