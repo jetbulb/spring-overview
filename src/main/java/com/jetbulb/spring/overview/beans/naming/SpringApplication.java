085557eb86bd4fd952749c515cf4265aa2561645
wespe <m.dobrynin@raccoonsoft.ru>
2021-11-15 20:34:51 +0100

    beans/naming/lesson-1: Java-configuration - simplify view

--- src/main/java/com/jetbulb/spring/overview/beans/naming/SpringApplication.java
+++ src/main/java/com/jetbulb/spring/overview/beans/naming/SpringApplication.java
@@ -4,10 +4,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class SpringApplication {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(JavaConfiguration.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                JavaConfiguration.class
+        );
 
 
-        context.getBeansOfType(BeanA.class).forEach((k, v) -> System.out.printf("Id: %s%nBean: %s%n%n", k, v));
+        context.getBeansOfType(BeanA.class)
+                .forEach((k, v) -> System.out.printf("Id: %s%nBean: %s%n%n", k, v));
 
         System.out.println("Bean with alias: aliasedTwinBean");
         System.out.println(context.getBean("aliasedTwinBean"));
